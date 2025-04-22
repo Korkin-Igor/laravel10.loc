1354c1546931138242c70c51d1ad8754061c33d5
ig0R <korkinigorttit@gmail.com>
2025-04-23 00:05:00 +0700

    Работа с БД. Query Builder

--- project/app/Http/Controllers/MainController.php
+++ project/app/Http/Controllers/MainController.php
@@ -10,26 +10,13 @@ class MainController extends Controller
 
     public function index(Request $request)
     {
-        $result = DB::select('select * from products where id = ?', [$request->route('id')]);
-        return response()->json($result);
+        $users = DB::table('users')->get(['id', 'name', 'email']);
+        $productsWithPriceLower700 = DB::table('products')->get(['title'])
+            ->where('price', '<', 700);
+        $products = DB::table('products')->get(['title', 'price'])
+            ->sortBy('price');
+        $products = DB::table('products')
+            ->orderByDesc('id')
+            ->get(['title', 'price']);
     }
-
-    public function addProduct(Request $request)
-    {
-        DB::insert('insert into products (title, description, price) values (?, ?, ?)', [$request->route('title'), $request->route('description'), $request->route('price')]);
-        return response()->json('Product added successfully', 201);
-    }
-
-    public function updateProductPrice(Request $request)
-    {
-        DB::update('update products set price = ? where id = ?', [$request->route('price'), $request->route('id')]);
-        return response()->json('Product updated successfully', 201);
-    }
-
-    public function deleteProduct(Request $request)
-    {
-        DB::delete('delete from products where id = ?', [$request->route('id')]);
-        return response()->json('Product deleted successfully', 201);
-    }
-
 }
